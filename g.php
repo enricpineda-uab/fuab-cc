ee496dd836aee5d37468c3926bca1e2b0d2a17f6
Enric Pineda <hola@enricpineda.cat>
2024-05-03 00:19:55 +0200

    Query diferent si és integer

--- g.php
+++ g.php
@@ -3,7 +3,11 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $c = $_GET['c'];
 $db = pg_connect("host=localhost dbname=fuabcc user=fuabcc password=15Maig1977!");
+if (gettype($c)=="integer"){
 $query = "select url from adreces where (id='$c' OR alias='$c')";
+} else {
+    $query = "select url from adreces where alias='$c'";
+}
 $result = pg_query($db,$query);
 $desti = pg_fetch_row($result,0);
 echo $desti[0];
