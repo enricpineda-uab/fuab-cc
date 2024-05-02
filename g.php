e273e2ba5b7c33a880a22db53463359eedf69dea
Enric Pineda <hola@enricpineda.cat>
2024-05-03 00:47:36 +0200

    Afegim check de variable si només és de dígits

--- g.php
+++ g.php
@@ -2,9 +2,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 $c = $_GET['c'];
+//$id = $_GET['id'];
 $db = pg_connect("host=localhost dbname=fuabcc user=fuabcc password=15Maig1977!");
-if (gettype($c)=="integer"){
-$query = "select url from adreces where (id='$c' OR alias='$c')";
+if (ctype_digit($c)=== true){
+$query = "select url from adreces where id='$c'";
 } else {
     $query = "select url from adreces where alias='$c'";
 }
