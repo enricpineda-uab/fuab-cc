af4091b4228a995eeb9e8a1abd8a141dbfbf699c
Enric Pineda <hola@enricpineda.cat>
2024-05-03 14:34:56 +0200

    Esmenem error en el CRUD

--- crud.php
+++ crud.php
@@ -3,7 +3,7 @@
 $json = file_get_contents('php://input');
 $dades = json_decode($json);
 
-$missatge = "Si ha anat bé, el mètode escollit és ".$dades=>metode;
+$missatge = "Si ha anat bé, el mètode escollit és ".$dades->metode;
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($missatge);
 ?>
