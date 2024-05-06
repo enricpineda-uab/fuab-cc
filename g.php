21f34f77be6b51eb847f0f6b67feb6e6c8803b13
Enric Pineda <hola@enricpineda.cat>
2024-05-06 11:50:03 +0200

    Canvis a les consultes del redireccionador

--- g.php
+++ g.php
@@ -5,9 +5,9 @@ $c = $_GET['c'];
 //$id = $_GET['id'];
 $db = pg_connect("host=localhost dbname=fuabcc user=fuabcc password=15Maig1977!");
 if (ctype_digit($c)=== true){
-$query = "select url from adreces where id='$c'";
+$query = "select id,url from adreces where id='$c'";
 } else {
-    $query = "select url from adreces where alias='$c'";
+    $query = "select id,url from adreces where alias='$c'";
 }
 $result = pg_query($db,$query);
 $desti = pg_fetch_row($result,0);
@@ -16,5 +16,5 @@ $result2 = pg_query($db,"INSERT INTO log (idurl,dataconsulta) VALUES ('".$desti[
 //echo $desti[0];
 if (!$result2) {echo "NO ho he executat";}
 pg_close($db);
-//header("Location: ".$desti[0]);
+header("Location: ".$desti[1]);
 ?>
